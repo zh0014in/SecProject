77b7b0368be9397c5e800f42af05aa62219e4e82
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2017-09-24 14:54:57 +0800

    traing & test

--- PacketAnalyzer.py
+++ PacketAnalyzer.py
@@ -12,21 +12,22 @@ ServerAddress = "192.168.0.111"
 ClientAddress = "192.168.0.168"
 
 
-def parseTrainingFile(fileName):
+def parseFile(fileName, output):
+    print("opening file " + fileName)
     doc = fitz.open(fileName)
     pageCount = doc.pageCount
-    for c in range(0, pageCount):
-        page = doc.loadPage(c)
-        text = page.getText("text")
-        buf = io.StringIO(text)
-        lines = buf.readlines()
-        # remove first two lines and the last two lines
-        lines = lines[2:-1] 
-        # filter out only server/client messages
-        lines = [l for l in lines if ClientAddress in l and ServerAddress in l]
+    with open(output, 'w',newline="\n", encoding="utf-8") as file:
+        writer = csv.writer(file)
         ts = 0
-        with open('training.csv', 'w',newline="\n", encoding="utf-8") as file:
-            writer = csv.writer(file)
+        for c in range(0, pageCount):
+            page = doc.loadPage(c)
+            text = page.getText("text")
+            buf = io.StringIO(text)
+            lines = buf.readlines()
+            # remove first two lines and the last two lines
+            lines = lines[2:-1] 
+            # filter out only server/client messages
+            lines = [l for l in lines if ClientAddress in l and ServerAddress in l]
             for line in lines:
                 split = re.split(r'\s{2,}',line)
                 split.append( float(split[0])-ts)
@@ -42,4 +43,5 @@ def parseTrainingFile(fileName):
                 print(split)
                 writer.writerow(split)
 
-parseTrainingFile("capture.xps");
+parseFile("capture.xps", "./training.csv");
+parseFile("test.xps", "test.csv")
