5945905169d5f80799049f2df50ab526f395b36c
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2017-09-30 15:51:26 +0800

    add client

---
+++ Client/Client/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    class Program : WebClient
+    {
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            do
+            {
+                p.Request();
+                System.Threading.Thread.Sleep(5000);
+            } while (true);
+            
+        }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            HttpWebRequest request = (HttpWebRequest)base.GetWebRequest(address);
+            request.KeepAlive = false;
+            request.ClientCertificates.Add(new X509Certificate2());
+            return request;
+        }
+
+        void Request()
+        {
+            string html = string.Empty;
+            string url = "https://192.168.0.111";
+            ServicePointManager.ServerCertificateValidationCallback = new System.Net.Security.RemoteCertificateValidationCallback(AcceptAllCertifications);
+
+            var request = GetWebRequest(new Uri(url));
+            
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                html = reader.ReadToEnd();
+            }
+
+            Console.WriteLine(html);
+
+        }
+
+        bool AcceptAllCertifications(object sender, System.Security.Cryptography.X509Certificates.X509Certificate certification, System.Security.Cryptography.X509Certificates.X509Chain chain, System.Net.Security.SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+    }
+}
