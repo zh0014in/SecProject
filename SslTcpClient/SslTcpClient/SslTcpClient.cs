23b217d275ebda43bf345933c940bb6351cd87f9
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2017-10-13 10:09:31 +0800

    ssl client .net 3.5

--- SslTcpClient/SslTcpClient/SslTcpClient.cs
+++ SslTcpClient/SslTcpClient/SslTcpClient.cs
@@ -1,13 +1,10 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net.Security;
 using System.Net.Sockets;
 using System.Security.Authentication;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace SslTcpClient
 {
