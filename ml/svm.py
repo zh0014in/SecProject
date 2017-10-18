972e1e3c4f3b4ee238b352bc94b2ceca67ff69be
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2017-10-18 14:39:36 +0800

    svm tune

--- ml/svm.py
+++ ml/svm.py
@@ -2,60 +2,56 @@ import numpy as np
 import pandas as pd
 from numpy.core.defchararray import index
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import classification_report
+from sklearn.svm import SVC
 
-train = pd.read_csv('../packets/train',sep=",")
+train = pd.read_csv('../packets/train', sep=",")
 data_train = np.array(train)
 
 i_train = data_train[:, 0]
-X_train = data_train[:, 5:6]  
-y_train = data_train[:, 7]
+X = data_train[:, 5:6]
+y = data_train[:, 7]
 
-print X_train
+print X
 
-from sklearn import svm
-    
-c = 500.0
-g = 0.01
-k = 'rbf'
-clf = svm.SVC(C=c, gamma=g, kernel=k)
-scores = cross_val_score(clf, iris.data, iris.target, cv=5)
+# Split the dataset in two equal parts
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.5, random_state=0)
 
-print clf.fit(X_train,y_train)
+# Set the parameters by cross-validation
+tuned_parameters = [{'kernel': ['rbf'], 'gamma': [1e-2, 1e-3, 1e-4],
+                     'C': [300, 400, 500, 700, 800, 900]}]
 
-score = clf.score(X_test,y_test)
-print(score)
+scores = ['recall']
 
-# result = clf.predict(X_test)
-#  
-# result = np.column_stack([i_test, result])
-# result = np.vstack([p_train, result])
-# print result
-# thefile = open('result.csv', 'w')
-# thefile.write("Id,Prediction\n")
-# for index, item in enumerate(result):
-#     thefile.write("%s" % item[0])
-#     thefile.write(",%s\n" % item[1])
+for score in scores:
+    print("# Tuning hyper-parameters for %s" % score)
+    print()
 
-# a = np.concatenate((i_train,i_test))
-# print a
-# for index,value in enumerate(a):
-#     print value
-    
-# print(i_train)
-# a = np.concatenate((i_train, y_train),axis = 0)
-# print(a)
-  
-#===============================================================================
-# thefile = open('result.csv', 'w')
-# thefile.write("Id,Prediction\n")
-# errorCount = 0
-# for index, item in enumerate(result):
-#     thefile.write("%s" % (index+1))
-#     thefile.write(",%s" % item)
-#     thefile.write(",%s\n" % y_train[index])
-#     if item != y_train[index]:
-#         errorCount = errorCount+1
-#           
-# thefile.write("%s, " % errorCount)
-# thefile.write("%s\n" % len(result))
-#===============================================================================
+    clf = GridSearchCV(SVC(C=100, cache_size=2000, probability=True), tuned_parameters, cv=5,
+                       scoring='%s_macro' % score, n_jobs=2)
+    clf.fit(X_train, y_train)
+
+    print("Best parameters set found on development set:")
+    print()
+    print(clf.best_params_)
+    print()
+    print("Grid scores on development set:")
+    print()
+    means = clf.cv_results_['mean_test_score']
+    stds = clf.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r"
+                % (mean, std * 2, params))
+    print()
+
+    print("Detailed classification report:")
+    print()
+    print("The model is trained on the full development set.")
+    print("The scores are computed on the full evaluation set.")
+    print()
+    y_true, y_pred = y_test, clf.predict(X_test)
+    print(classification_report(y_true, y_pred))
+    print()
