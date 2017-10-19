b7137d08cf203823a0feab0dcb4fe1743281300d
Zhang Mingmin <zh0014in@e.ntu.edu.sg>
2017-10-19 10:20:30 +0800

    add adaboost

---
+++ ml/adaBoost.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import cross_val_score
+from sklearn.ensemble import AdaBoostClassifier
+
+train = pd.read_csv('../packets/train', sep=",")
+data_train = np.array(train)
+
+i_train = data_train[:, 0]
+X = data_train[:, 5:6]
+y = data_train[:, 7]
+
+clf = AdaBoostClassifier(n_estimators=100)
+scores = cross_val_score(clf, X, y)
+scores.mean()
